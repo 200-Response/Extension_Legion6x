9dc8fe905b41e052064f836166c369175a9acf73
Carlos Cancines <Carlozc90@gmail.com>
2022-10-13 11:42:10 -0500

    finish color

--- slider.js
+++ slider.js
@@ -17,8 +17,16 @@ const parraf = document.getElementById('parraf');
 slider.addEventListener('mousemove', function () {
   // var x = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
   let x = slider.value;
-  let color = `linear-gradient(90deg, rgb(117,252,117) ${x}%, rgb(214,214,214) ${x}%)`;
-  slider.style.background = color;
+  // let color = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(0,221,3)`;
+  // slider.style.background = color;
+
+  if (x > 75) {
+    slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(0,221,3)`;
+  } else if (x > 50) {
+    slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(214,214,214) ${x}%)`;
+  } else if (x > 20) {
+    slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(214,214,214) ${x}%)`;
+  }
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -28,40 +36,46 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-const color = (x) => {
-  let color = `linear-gradient(90deg, rgb(117,252,117) ${x}%, rgb(214,214,214) ${x}%)`;
-  slider.style.background = color;
-  slider.value = x;
-};
+// const color = (x) => {
+//   let color = `linear-gradient(90deg, rgb(117,252,117) ${x}%, rgb(214,214,214) ${x}%)`;
+//   slider.style.background = color;
+//   slider.value = x;
+// };
 
 chrome.runtime.sendMessage({ name: 'fetchWords' }, (response) => {
   switch (response?.number) {
     case 1:
       //red
-      // bton.innerHTML = parraf;
       yellowFace.style.filter = 'grayscale(100%)';
       greenFace.style.filter = 'grayscale(100%)';
-      color(20);
+
+      slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(214,214,214) ${20}%)`;
+      slider.value = 20;
+
       parraf.innerHTML = ` <p id="parraf">
       Este sitio <span>sí</span> contiene <span>sesgo de genero</span>
     </p>`;
       break;
     case 2:
       //yellow
-      // bton.innerHTML = '2';
       redFace.style.filter = 'grayscale(100%)';
       greenFace.style.filter = 'grayscale(100%)';
-      color(50);
+
+      slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(214,214,214) ${50}%)`;
+      slider.value = 50;
+
       parraf.innerHTML = ` <p id="parraf">
       Este sitio <span>puede</span> contener <span>sesgo de genero</span>
     </p>`;
       break;
     case 3:
       //green
-      // bton.innerHTML = '3';
       redFace.style.filter = 'grayscale(100%)';
       yellowFace.style.filter = 'grayscale(100%)';
-      color(100);
+
+      slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(0,221,3)`;
+      slider.value = 100;
+
       parraf.innerHTML = ` <p id="parraf">
       Este sitio <span>no</span> contiene <span>sesgo de genero</span>
     </p>`;
