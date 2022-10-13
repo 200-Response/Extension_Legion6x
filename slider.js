368d61f81ffaefec68dc2552e344a4fd116603af
Carlos Cancines <Carlozc90@gmail.com>
2022-10-12 23:02:07 -0500

    hiden faces

--- slider.js
+++ slider.js
@@ -1,16 +1,24 @@
 const slider = document.getElementById('myRange');
-const output = document.getElementById('value');
+// const btnOk = document.getElementById('buttonOk');
+// const output = document.getElementById('value');
 
-output.innerHTML = slider.value;
+// output.innerHTML = slider.value;
 
-slider.oninput = function () {
-  output.innerHTML = this.value;
-};
+// slider.oninput = function () {
+//   output.innerHTML = this.value;
+// };
 
 slider.addEventListener('mousemove', function () {
   // var x = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
   let x = slider.value;
-  //prettier-ignore
-  var color =`linear-gradient(90deg, rgb(117,252,117) ${x}%, rgb(214,214,214) ${x}%)`;
+  let color = `linear-gradient(90deg, rgb(117,252,117) ${x}%, rgb(214,214,214) ${x}%)`;
   slider.style.background = color;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  let bton = document.getElementById('buttonOk');
+  bton.addEventListener('click', function () {
+    // link.innerHTML = 'test';
+    window.close();
+  });
 });
