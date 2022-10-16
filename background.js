308133f5471957fa97107c57e03f9de39dc3ef7c
Carlos Cancines <Carlozc90@gmail.com>
2022-10-15 19:34:39 -0500

    Api heroku

--- background.js
+++ background.js
@@ -1,53 +1,8 @@
-function getRandomNumber(number) {
-  let max = number + 1;
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
 function getColor(str) {
-  if (str === 'red') return './assets/60x60/red-60x60.png';
-  if (str === 'yellow') return './assets/60x60/yellow-60x60.png';
-  if (str === 'green') return './assets/60x60/green-60x60.png';
+  if (str === 'red') return './assets/30x30/red-30x30.png';
+  if (str === 'yellow') return './assets/30x30/yellow-30x30.png';
+  if (str === 'green') return './assets/30x30/green-30x30.png';
 }
-//test
-// test
-
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
-  if (msg.name === 'fetchWords') {
-    const pokem = [
-      'bulbasaur',
-      'squirtle',
-      'pidgey',
-      'ekans',
-      'nidorina',
-      'ninetales',
-      'psyduck',
-    ];
-
-    const number = getRandomNumber(3);
-
-    let name = pokem[number];
-
-    const apiCall = 'https://pokeapi.co/api/v2/pokemon/' + name;
-    console.log('api', apiCall);
-
-    fetch(apiCall)
-      .then(function (res) {
-        if (res.status !== 200) {
-          response({ word: 'Error', desc: 'Problema al cargar el api2' });
-          return;
-        }
-        res.json().then(function (data) {
-          response({ word: data, number });
-        });
-      })
-      .catch(function (err) {
-        response({ word: 'Error', desc: 'Problema al cargar el api1' });
-      });
-
-    // response({ api: apiCall, nombre: name });
-  }
-  return true;
-});
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
   if (msg.name === 'fetchWords') {
@@ -55,6 +10,21 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
       let url = tabs[0].url;
       console.log('url', encodeURIComponent(url));
       // use `url` here inside the callback because it's asynchronous!
+      const apiCall = `https://fastapi-sesgo.herokuapp.com/${url}`;
+      fetch(apiCall)
+        .then(function (res) {
+          if (res.status !== 200) {
+            response({ word: 'Error', desc: 'Problema al cargar el api2' });
+            return;
+          }
+          res.json().then(function (data) {
+            response({ number: data.random });
+            console.log('data', data);
+          });
+        })
+        .catch(function (err) {
+          response({ word: 'Error', desc: 'Problema al cargar el api1' });
+        });
     });
   }
 
--- slider.js
+++ slider.js
@@ -1,94 +1,54 @@
 const slider = document.getElementById('myRange');
-let bton = document.getElementById('buttonOk');
+const bton = document.getElementById('buttonOk');
 const redFace = document.getElementById('redFace');
 const yellowFace = document.getElementById('yellowFace');
 const greenFace = document.getElementById('greenFace');
 const parraf = document.getElementById('parraf');
 
-// const btnOk = document.getElementById('buttonOk');
-// const output = document.getElementById('value');
-
-// output.innerHTML = slider.value;
-
-// slider.oninput = function () {
-//   output.innerHTML = this.value;
-// };
-
-slider.addEventListener('mousemove', function () {
-  // var x = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
-  let x = slider.value;
-  // let color = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(0,221,3)`;
-  // slider.style.background = color;
-
-  if (x > 75) {
-    // slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(0,221,3)`;
-  } else if (x > 50) {
-    // slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(214,214,214) ${x}%)`;
-  } else if (x > 20) {
-    // slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(214,214,214) ${x}%)`;
-  }
-});
-
 document.addEventListener('DOMContentLoaded', function () {
   bton.addEventListener('click', function () {
-    // link.innerHTML = 'test';
     window.close();
   });
 });
 
-// const color = (x) => {
-//   let color = `linear-gradient(90deg, rgb(117,252,117) ${x}%, rgb(214,214,214) ${x}%)`;
-//   slider.style.background = color;
-//   slider.value = x;
-// };
-
 chrome.runtime.sendMessage({ name: 'fetchWords' }, (response) => {
-  switch (response?.number) {
-    case 1:
-      //red
-      yellowFace.style.filter = 'grayscale(100%)';
-      greenFace.style.filter = 'grayscale(100%)';
+  parraf.innerHTML = JSON.stringify(response);
 
-      slider.style.background = `linear-gradient(90deg, rgb(0,221,3), rgb(249,205,53), rgb(213,0,0)`;
-      // slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(214,214,214) ${20}%)`;
-      slider.value = 100;
-
-      parraf.innerHTML = ` <p id="parraf">
-      Este sitio <span>sí</span> contiene <span>sesgo de genero</span>
-    </p>`;
-
-      // chrome.browserAction.setIcon({ path: './assets/60x60/red-60x60.png' });
-      chrome.runtime.sendMessage({ icon1: 'red' });
-      break;
-    case 2:
-      //yellow
-      redFace.style.filter = 'grayscale(100%)';
-      greenFace.style.filter = 'grayscale(100%)';
-
-      slider.style.background = `linear-gradient(90deg, rgb(0,221,3), rgb(249,205,53), rgb(214,214,214) ${50}%)`;
-      slider.value = 50;
-
-      parraf.innerHTML = ` <p id="parraf">
-      Este sitio <span>puede</span> contener <span>sesgo de genero</span>
-    </p>`;
-      chrome.runtime.sendMessage({ icon1: 'yellow' });
-      break;
-    case 3:
-      //green
-      redFace.style.filter = 'grayscale(100%)';
-      yellowFace.style.filter = 'grayscale(100%)';
-
-      // slider.style.background = `linear-gradient(90deg, rgb(213,0,0), rgb(249,205,53), rgb(0,221,3)`;
-      slider.style.background = `linear-gradient(90deg, rgb(0,221,3), rgb(214,214,214) ${20}%)`;
-      slider.value = 20;
-
-      parraf.innerHTML = ` <p id="parraf">
+  if (response.number < 0.3) {
+    //green
+    redFace.style.filter = 'grayscale(100%)';
+    yellowFace.style.filter = 'grayscale(100%)';
+    slider.style.background = `linear-gradient(90deg, rgb(0,221,3), rgb(214,214,214) ${20}%)`;
+    slider.value = 20;
+    parraf.innerHTML = ` <p id="parraf">
       Este sitio <span>no</span> contiene <span>sesgo de genero</span>
     </p>`;
-      chrome.runtime.sendMessage({ icon1: 'green' });
-      break;
+    chrome.runtime.sendMessage({ icon1: 'green' });
+    return;
+  }
 
-    default:
-      break;
+  if (response.number >= 0.3 && response.number <= 0.7) {
+    //yellow
+    redFace.style.filter = 'grayscale(100%)';
+    greenFace.style.filter = 'grayscale(100%)';
+    slider.style.background = `linear-gradient(90deg, rgb(0,221,3), rgb(249,205,53), rgb(214,214,214) ${50}%)`;
+    slider.value = 50;
+    parraf.innerHTML = ` <p id="parraf">
+      Este sitio <span>puede</span> contener <span>sesgo de genero</span>
+    </p>`;
+    chrome.runtime.sendMessage({ icon1: 'yellow' });
+    return;
+  }
+
+  if (response.number > 0.7) {
+    //red
+    yellowFace.style.filter = 'grayscale(100%)';
+    greenFace.style.filter = 'grayscale(100%)';
+    slider.style.background = `linear-gradient(90deg, rgb(0,221,3), rgb(249,205,53), rgb(213,0,0)`;
+    slider.value = 100;
+    parraf.innerHTML = ` <p id="parraf">
+   Este sitio <span>sí</span> contiene <span>sesgo de genero</span>
+ </p>`;
+    chrome.runtime.sendMessage({ icon1: 'red' });
   }
 });
